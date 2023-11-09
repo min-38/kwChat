6670baefd9ea21cbc856de75aebc27e99447a96c
min-38 <mindino03@gmail.com>
2023-11-09 12:51:15 +0900

    #10_프로필 뷰 템플릿 추가
    - 로그아웃 버튼 및 동작 적용

--- app/src/screens/Profile.jsx
+++ app/src/screens/Profile.jsx
@@ -1,10 +1,100 @@
-const { SafeAreaView, Text } = require("react-native");
+const { View, Text, Image, TouchableOpacity, Alert } = require("react-native");
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import useGlobal from "../core/global"
+import utils from "../core/utils"
+
+function ProfileLogout() {
+    const logout = useGlobal(state => state.logout)
+
+    goAlert = () =>
+        Alert.alert("로그아웃할까요?", '',
+            [
+                { text: "아니요", onPress: () => {}, style: 'cancel' },
+                {
+                    text: "로그아웃",
+                    onPress: logout,
+                    style: 'destructive'
+                }
+                
+            ],
+            {
+                cancelable: true,
+                onDismiss: () => {},
+            },
+        )
+
+    return (
+        <TouchableOpacity
+            onPress={ this.goAlert }
+            style={{ 
+                flexDirection: 'row',
+                height: 52,
+                borderRadius: 26,
+                alignItems: 'center',
+                justifyContent: 'center',
+                paddingHorizontal: 26,
+                backgroundColor: '#202020',
+                marginTop: 40
+            }}
+        >
+            <FontAwesomeIcon
+				icon='right-from-bracket'
+				size={20}
+				color='#d0d0d0'
+				style={{ marginRight: 12}}
+			/>
+            <Text
+                style={{ 
+                    fontWeight: 'bold',
+                    color: "#d0d0d0",
+
+                }}
+            >
+                로그아웃
+            </Text>
+        </TouchableOpacity>
+    )
+}
 
 function ProfileScreen() {
     return (
-        <SafeAreaView>
-            <Text>Profile</Text>
-        </SafeAreaView>
+        <View
+            style={{
+                flex: 1,
+                alignItems: 'center',
+                paddingTop: 100
+            }}
+        >
+            <Image
+                source={require('../assets/images/default-user-avartar.png')}
+                style={{ width: 180, height: 180, borderRadius: 90, backgroundColor: '#e0e0e0', marginBottom: 20 }}
+            />
+            <Text
+                style={{ 
+                    textAlign: 'center',
+                    color: '#303030',
+                    fontSize: 20,
+                    fontWeight: 'bold',
+                    marginTop: 6,
+                }}
+            >
+                사용자
+            </Text>
+
+            <Text
+                style={{ 
+                    textAlign: 'center',
+                    color: '#606060',
+                    fontSize: 14,
+                    // fontWeight: 'bold',
+                    // marginTop: 6,
+                }}
+            >
+                @사용자
+            </Text>
+            
+            <ProfileLogout />
+        </View>
     )
 }
 
